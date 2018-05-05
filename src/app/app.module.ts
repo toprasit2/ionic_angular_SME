9ae05e1f2e8c8ddf6766b05e82b8e97233954517
toprasit2 <toprasit.gk@gmail.com>
2018-05-05 13:06:08 +0700

    create component calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
