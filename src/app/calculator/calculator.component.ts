e3e3cea1b855b91d821ce7a4d17319623c931992
toprasit2 <toprasit.gk@gmail.com>
2018-05-05 13:33:20 +0700

    create event blinding and property blinding

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-
+  isAllowed = true;
+  result:any = 0;
   constructor() { }
 
   ngOnInit() {
   }
 
+  calculate(){
+    console.log("Clicked");
+    this.result += 1;
+  }
 }
