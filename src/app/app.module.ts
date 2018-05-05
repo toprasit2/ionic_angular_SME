fe5eab879717dbcad3d62ae2fad9d4c57a04a799
toprasit2 <toprasit.gk@gmail.com>
2018-05-05 15:29:36 +0700

    add Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { UserComponent } from './user/user.component';
 import { HighlightDirective } from './highlight.directive';
 import { HomeComponent } from './home/home.component';
+
+import { Routes, RouterModule} from '@angular/router'
+
+const my_route: Routes = [
+  {path: "", component:HomeComponent},
+  {path: "calculator", component:CalculatorComponent},
+  {path: "user", component:UserComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,7 +27,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule,
+    RouterModule.forRoot(my_route)
   ],
   providers: [],
   bootstrap: [AppComponent]
