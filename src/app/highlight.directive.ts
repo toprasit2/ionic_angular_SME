bcfdde7b4503b83d082d2781b79f2b946d92e00a
toprasit2 <toprasit.gk@gmail.com>
2018-05-06 09:59:01 +0700

    finish

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 export class HighlightDirective {
  
  @Input("appHighlight") status: string;
- 
+ @Input("test") test: string;
  constructor(private el: ElementRef) { }
  
  @HostListener('mouseenter') onMouseEnter() {
@@ -18,12 +18,16 @@ export class HighlightDirective {
  }
  
  private highlight() {
-   if(this.status == 'single'){
+   if(this.status == 'single' && this.test == '0'){
      this.el.nativeElement.style.backgroundColor = 'lightgreen';
      this.el.nativeElement.style.fontSize = '50px';
    }else if(this.status == 'married'){
      this.el.nativeElement.style.backgroundColor = 'pink';
      this.el.nativeElement.style.fontSize = '50px';
+   }
+   else{
+    this.el.nativeElement.style.backgroundColor = 'lightblue';
+    this.el.nativeElement.style.fontSize = '50px';
    }
  }
  
